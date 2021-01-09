816147c1381a1ed4b3c118dfa60b79212eafdc2b
ccxin233 <1428604355@qq.com>
2021-01-09 11:49:04 +0800

    递归

--- src/main/java/online/cx/javabasic/leetcode/day/Fibonacci.java
+++ src/main/java/online/cx/javabasic/leetcode/day/Fibonacci.java
@@ -2,16 +2,10 @@ package online.cx.javabasic.leetcode.day;
 
 public class Fibonacci {
     public int fib(int n) {
-        if (n < 2) {
-            return n;
+        if (n == 0 || n == 1) {
+            return 0;
         }
-        int p = 0, q = 0, r = 1;
-        for (int i = 2; i <= n; ++i) {
-            p = q;
-            q = r;
-            r = p + q;
-        }
-        return r;
+        return fib(n - 1 ) + fib(n - 2);
     }
 
     public static void main(String[] args) {
