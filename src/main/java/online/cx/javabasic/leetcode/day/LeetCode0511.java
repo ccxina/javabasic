cc18793c1971b46384300dcd651404faf7fffd45
caoxin <1428604355@qq.com>
2021-05-11 17:41:06 +0800

    # leet code

--- src/main/java/online/cx/javabasic/leetcode/day/LeetCode0511.java
+++ src/main/java/online/cx/javabasic/leetcode/day/LeetCode0511.java
@@ -85,6 +85,35 @@ public class LeetCode0511 {
         return !sMap.isEmpty();
     }
 
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
     public static void main(String[] args) {
         int[] encode = new int[]{3,1};
         int[] res = decode(encode);
