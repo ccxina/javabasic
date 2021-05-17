6f06a44fbaf0e09965d6e83ece5b8c68943d36ac
caoxin <1428604355@qq.com>
2021-05-17 10:16:58 +0800

    # 反转链表

--- src/main/java/online/cx/javabasic/leetcode/day/may/LeetCode14.java
+++ src/main/java/online/cx/javabasic/leetcode/day/may/LeetCode14.java
@@ -4,6 +4,7 @@ package online.cx.javabasic.leetcode.day.may;
 import online.cx.javabasic.leetcode.utils.ListNode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -55,6 +56,27 @@ public class LeetCode14 {
         return pre;
     }
 
+    public static ListNode createNode(int[] nums) {
+        Arrays.sort(nums);
+        ListNode pre = new ListNode(nums[0]);
+        ListNode cur = pre;
+        for (int i = 1; i < nums.length; i++) {
+            cur.next = new ListNode(nums[i]);
+            cur = cur.next;
+        }
+        return pre;
+    }
+
+    public static void foreach(ListNode root) {
+        System.out.print("[");
+        while (root != null) {
+            System.out.print(root.val);
+            System.out.print("\t");
+            root = root.next;
+        }
+        System.out.print("]");
+        System.out.println();
+    }
     public static ListNode reverseList1(ListNode head) {
         ListNode pre = null;
         ListNode cur = head;
@@ -67,9 +89,48 @@ public class LeetCode14 {
         return pre;
     }
 
+    /**
+     * 合并链表
+     */
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(0);
+        ListNode cur = res;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                cur.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return res.next;
+    }
+
+
+    public static ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        else if (l2 == null) return l1;
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists1(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists1(l1, l2.next);
+            return l2;
+        }
+    }
     public static void main(String[] args) {
 //        System.out.println(intToRoman(1994));
-        ListNode head = new ListNode(1);
-        ListNode res = reverseList(head);
+        ListNode node1 = createNode(new int[]{5, 4, 3, 2, 1});
+        foreach(node1);
+        ListNode node2 = createNode(new int[]{5, 4, 3, 2, 1});
+        foreach(node2);
+        ListNode node3 = mergeTwoLists(node1, node2);
+        ListNode node4 = mergeTwoLists1(node1, node2);
+        foreach(node3);
+        foreach(node4);
+
     }
 }
