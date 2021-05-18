5af1bbfbd34ba1caa8cc161573527b5d5ccbecaf
caoxin <1428604355@qq.com>
2021-05-18 09:19:55 +0800

    # 二叉树的堂兄弟节点

---
+++ src/main/java/online/cx/javabasic/leetcode/day/may/LeetCode0517.java
@@ -0,0 +1,52 @@
+package online.cx.javabasic.leetcode.day.may;
+
+import online.cx.javabasic.leetcode.utils.TreeNode;
+
+/**
+ * @author Cao Xin
+ * @since 2021/5/17
+ */
+public class LeetCode0517 {
+
+    // x 的信息
+    int x;
+    TreeNode xParent;
+    int xDepth;
+    boolean xFound = false;
+
+    // y 的信息
+    int y;
+    TreeNode yParent;
+    int yDepth;
+    boolean yFound = false;
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        this.x = x;
+        this.y = y;
+        dfs(root, 0, null);
+        return xDepth == yDepth && xParent != yParent;
+    }
+
+    private void dfs(TreeNode node, int depth, TreeNode parent) {
+        if (node == null) {return;}
+        if (node.val == x) {
+            xParent = parent;
+            xDepth = depth;
+            xFound = true;
+        } else if (node.val == y) {
+            yParent = parent;
+            yDepth = depth;
+            yFound = true;
+        }
+        if (xFound && yFound) {
+            return;
+        }
+
+        dfs(node.left, depth+1, node);
+        if (xFound && yFound) {
+            return;
+        }
+        dfs(node.right, depth+1, node);
+    }
+
+}
